1b547c2a323ec2eba24efbf2188bc4d84dbb21f5
Marc Balestreri <hello@marcbal.com>
2025-03-29 15:47:29 -0700

    Added Editor Agent for Editing Content

--- main.py
+++ main.py
@@ -45,3 +45,19 @@ writer = Agent(
     allow_delegation=False,
     verbose=True
 )
+
+editor = Agent(
+    role="Editor",
+    goal="Edit a given blog post to align with "
+         "the writing style of the organization. ",
+    backstory="You are an editor who receives a blog post "
+              "from the Content Writer. "
+              "Your goal is to review the blog post "
+              "to ensure that it follows journalistic best practices,"
+              "provides balanced viewpoints "
+              "when providing opinions or assertions, "
+              "and also avoids major controversial topics "
+              "or opinions when possible.",
+    allow_delegation=False,
+    verbose=True
+)
